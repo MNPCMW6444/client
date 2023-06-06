c92692c9a43f62bac6f311419dd2d04a8b8778fb
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-06-06 10:28:53 +0300

    Update AI.tsx

--- src/app/components/pages/ai/AI.tsx
+++ src/app/components/pages/ai/AI.tsx
@@ -4,37 +4,39 @@ import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import { TreeItem, TreeView } from "@mui/lab";
 import MainserverContext from "../../../context/WhiteserverContext";
 
+interface TreeNode {
+  name: string;
+  children: TreeNode[];
+}
+
 const AI = () => {
   const { axiosInstance } = useContext(MainserverContext);
 
-  const [tree, setTree] = useState({});
+  const [tree, setTree] = useState<TreeNode>();
 
   useEffect(() => {
     const fetchTree = async () => {
-      const { data } = await axiosInstance.get("ai/xxx");
-      setTree(data.order);
+      const { data } = await axiosInstance.get("ai/getPromptTree");
+      setTree(data.tree);
     };
     fetchTree();
-  }, []);
+  }, [axiosInstance]);
 
-  console.log(tree);
+  const nodeRenderer = (node: TreeNode | undefined) =>
+    node && (
+      <TreeItem nodeId={`${Math.random()}`} label={node.name}>
+        {node.children.map((node: TreeNode) => nodeRenderer(node))}
+      </TreeItem>
+    );
 
   return (
     <TreeView
       aria-label="file system navigator"
       defaultCollapseIcon={<ExpandMoreIcon />}
       defaultExpandIcon={<ChevronRightIcon />}
-      sx={{ height: 240, flexGrow: 1, maxWidth: 400, overflowY: "auto" }}
+      sx={{ height: 1000, flexGrow: 1, maxWidth: 400, overflowY: "auto" }}
     >
-      <TreeItem nodeId="1" label="Applications">
-        <TreeItem nodeId="2" label="Calendar" />
-      </TreeItem>
-      <TreeItem nodeId="5" label="Documents">
-        <TreeItem nodeId="10" label="OSS" />
-        <TreeItem nodeId="6" label="MUI">
-          <TreeItem nodeId="8" label="index.js" />
-        </TreeItem>
-      </TreeItem>
+      {nodeRenderer(tree)}
     </TreeView>
   );
 };
