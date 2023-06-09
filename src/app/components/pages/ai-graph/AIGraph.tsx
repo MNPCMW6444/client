11aa0549bd66a112c24209224f3236addc78f17d
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-06-09 16:54:07 +0300

    Update AIGraph.tsx

--- src/app/components/pages/ai-graph/AIGraph.tsx
+++ src/app/components/pages/ai-graph/AIGraph.tsx
@@ -3,7 +3,62 @@ import { Grid, Select, MenuItem, Typography, Paper } from "@mui/material";
 import MainserverContext from "../../../context/WhiteserverContext";
 import UserContext from "../../../context/UserContext";
 import PromptEditor from "./PromptEditor";
-import { PromptGraph } from "@failean/shared-types";
+import { PromptGraph, PromptMap, PromptPart } from "@failean/shared-types";
+
+const x = (promptMap: PromptMap) => {
+  let superPrompts = Object.keys(promptMap).map((promptName: string) => ({
+    name: promptName,
+    deps: promptMap[promptName]
+      .map(
+        (promptPart: PromptPart) =>
+          promptPart.type === "variable" && promptPart.content
+      )
+      .filter((x) => x) as string[],
+    level: 0,
+  }));
+  superPrompts.unshift({ name: "idea", deps: [], level: 0 });
+  let level = 0;
+  while (superPrompts.filter(({ level }) => level < 0).length > 0) {
+    level++;
+    superPrompts
+      .filter(({ level }) => !level)
+      .forEach((sp, index) => {
+        sp.level = level - 1;
+        superPrompts = [...superPrompts, sp];
+        superPrompts.splice(index, 1);
+      });
+    superPrompts
+      .filter(({ level }) => !level)
+      .forEach((sp: any, index: number) => {
+        let satisfied = sp.deps
+          .map(
+            (name: string) =>
+              superPrompts.find((spx) => spx.name === name)?.name
+          )
+          .map((name: string) => {
+            const number = superPrompts.find(
+              (spxx) => spxx.name === name
+            )?.level;
+            debugger;
+            return !name || (number && number > 0);
+          });
+        let total = true;
+        satisfied.forEach((f: boolean) => {
+          if (!f) total = false;
+        });
+        if (total) {
+          sp.level = -1;
+          superPrompts = [...superPrompts, sp];
+          superPrompts.splice(index, 1);
+        }
+      });
+  }
+  let graph = superPrompts.map(({ name, level }) => ({
+    name,
+    level,
+  }));
+  return graph;
+};
 
 const AIGraph = () => {
   const { axiosInstance } = useContext(MainserverContext);
@@ -14,6 +69,7 @@ const AIGraph = () => {
   useEffect(() => {
     const fetchGraph = async () => {
       const { data } = await axiosInstance.get("data/getPromptGraph");
+      x(data.graph);
       setGraph(data.graph);
     };
     fetchGraph();
