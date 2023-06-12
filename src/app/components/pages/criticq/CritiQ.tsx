cf01362b9b87e75a46758dbfb308856343c5ed53
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-06-12 21:44:46 +0300

    Update CritiQ.tsx

--- src/app/components/pages/criticq/CritiQ.tsx
+++ src/app/components/pages/criticq/CritiQ.tsx
@@ -1,30 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { Select } from 'antd';
-import { Grid, Paper,Button } from '@mui/material';
-
-type Idea = {
-  _id: string;
-  name: string;
-};
-
-type ActionPlanItem = {
-  id: string;
-  description: string;
-};
-
-const { Option } = Select;
+import { useState, useEffect, useContext } from "react";
+import { Grid, Paper, Button, Select, MenuItem } from "@mui/material";
+import UserContext from "../../../context/UserContext";
+import MainserverContext from "../../../context/WhiteserverContext";
+import { Prompt, PromptGraph } from "@failean/shared-types";
 
 const CritiQ = () => {
-  const [ideas, setIdeas] = useState<Idea[]>([]);
-  const [selectedIdea, setSelectedIdea] = useState('');
-  const [selectedPrompts, setSelectedPrompts] = useState<string[]>([]);
-  const [actionPlan, setActionPlan] = useState<ActionPlanItem[]>([]);
+  const { ideas } = useContext(UserContext);
+  const { axiosInstance } = useContext(MainserverContext);
+  const [selectedIdeaId, setSelectedIdeaId] = useState<string>("");
+  const [selectedPrompt, setSelectedPrompt] = useState<string>("");
+  const [selectedPromptsNames, setSelectedPromptsNames] = useState<string[]>(
+    []
+  );
+  const [actionPlan, setActionPlan] = useState<any>([]);
+  const [prompts, setPrompts] = useState<string[]>([]);
 
   useEffect(() => {
-    fetch('http://localhost:6555/api/critique/data/getIdeas')
-      .then((response) => response.json())
-      .then((data) => setIdeas(data));
-  }, []);
+    const fetchPrompts = async () => {
+      const { data } = await axiosInstance.get("data/getPromptGraph");
+      setPrompts((data.graph as PromptGraph).map(({ name }) => name));
+    };
+    fetchPrompts();
+  }, [axiosInstance]);
 
   const renderPromptOptions = () => {
     // Render your prompt options here
@@ -39,25 +36,28 @@ const CritiQ = () => {
     <Grid container spacing={3}>
       <Grid item>
         <Select
-          value={selectedIdea}
-          onChange={(value) => setSelectedIdea(value)}
-          style={{ width: 200 }}
+          value={selectedIdeaId}
+          onChange={(e) => setSelectedIdeaId(e.target.value)}
+          fullWidth
         >
-          {ideas.map((idea: Idea) => (
-            <Option key={idea._id} value={idea._id}>
-              {idea.name}
-            </Option>
+          {ideas.map((idea: any, index: number) => (
+            <MenuItem key={index} value={idea._id}>
+              {idea?.idea}
+            </MenuItem>
           ))}
         </Select>
       </Grid>
       <Grid item>
         <Select
-          mode="multiple"
-          style={{ width: 200 }}
-          placeholder="Select prompts"
-          onChange={(values: string[]) => setSelectedPrompts(values)}
+          value={selectedPrompt}
+          onChange={(e) => setSelectedPrompt(e.target.value)}
+          fullWidth
         >
-          {renderPromptOptions()}
+          {prompts.map((prompt: string, index: number) => (
+            <MenuItem key={index} value={prompt}>
+              {prompt}
+            </MenuItem>
+          ))}
         </Select>
       </Grid>
       <Grid item>
@@ -65,11 +65,12 @@ const CritiQ = () => {
       </Grid>
       <Grid item>
         <Paper elevation={2}>
-          {actionPlan.map((item: ActionPlanItem, index: number) => (
-            // Make sure to return a component or a piece of JSX here
-            // Replace 'null' with your actual component or JSX
-            null
-          ))}
+          {actionPlan.map(
+            (item: any, index: number) =>
+              // Make sure to return a component or a piece of JSX here
+              // Replace 'null' with your actual component or JSX
+              null
+          )}
         </Paper>
       </Grid>
     </Grid>
