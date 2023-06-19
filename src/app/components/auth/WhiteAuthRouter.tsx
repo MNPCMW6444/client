8a6b89f0949046f7c0746264313fae606383647a
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-06-19 13:39:27 +0300

    good

--- src/app/components/auth/WhiteAuthRouter.tsx
+++ src/app/components/auth/WhiteAuthRouter.tsx
@@ -3,11 +3,10 @@ import Login from "./Login";
 import Register from "./Register";
 import Reset from "./Reset";
 import Checkemail from "./Checkemail";
-import Website from "./Website";
 
 const WhiteAuthRouter = () => (
   <Routes>
-    <Route path="/*" element={<Website />} />
+    <Route path="/*" element={<Login />} />
     <Route path="/login" element={<Login />} />
     <Route path="/register" element={<Register />} />
     <Route path="/reset" element={<Reset />} />
