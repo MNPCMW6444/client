bce138d5458f1f709c6ce19bb5e333a3479c36a0
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-06-23 18:25:36 +0300

    Update AIDeator.tsx

--- src/app/components/pages/aideator/AIDeator.tsx
+++ src/app/components/pages/aideator/AIDeator.tsx
@@ -3,7 +3,9 @@ import { Grid, Typography, Paper } from "@mui/material";
 import { MainserverContext } from "@failean/mainserver-provider";
 import UserContext from "../../../context/UserContext";
 import Prompt from "../../common/Prompt";
-import PromptDialog from "../../common/prompt-dialog/PromptDialog";
+import PromptDialog, {
+  capitalize,
+} from "../../common/prompt-dialog/PromptDialog";
 import { PromptGraph, PromptName } from "@failean/shared-types";
 import IdeaSelector from "../../common/IdeaSelector";
 
@@ -14,7 +16,14 @@ const AIDeator = () => {
   const [currentIdeaId, setCurrentIdeaId] = useState<string>(
     ideas[0]?._id || ""
   );
-  const [graph, setGraph] = useState<PromptGraph>();
+
+  const [loaded, setLoaded] = useState<string>("");
+
+  interface Graph extends PromptGraph {
+    hasData: boolean;
+  }
+
+  const [graph, setGraph] = useState<Graph>();
 
   const [openPrompt, setOpenPrompt] = useState<PromptName | "closed">("closed");
 
@@ -22,11 +31,30 @@ const AIDeator = () => {
     const fetchGraph = async () => {
       if (axiosInstance) {
         const { data } = await axiosInstance.get("data/prompts/getPromptGraph");
-        setGraph(data.graph);
+        const baseGraph = data.graph;
+        let hasDatas: boolean[] = [];
+        for (const prompt of baseGraph) {
+          setLoaded(capitalize(prompt.name));
+          hasDatas.push(
+            (
+              await axiosInstance.post("data/prompts/getPromptResult", {
+                ideaId: currentIdeaId,
+                promptName: prompt.name,
+              })
+            ).data.promptResult?.data?.length > 2
+          );
+        }
+        setGraph(
+          baseGraph.map((x: any, index: number) => ({
+            ...x,
+            hasData: hasDatas[index],
+          }))
+        );
       }
     };
+    setGraph(undefined);
     fetchGraph();
-  }, [axiosInstance]);
+  }, [axiosInstance, currentIdeaId]);
 
   const renderGraph = (graph: PromptGraph) => {
     const result: any[][] = [];
@@ -92,7 +120,11 @@ const AIDeator = () => {
         </Grid>
         <Grid item>
           <Paper sx={{ overflow: "scroll" }}>
-            {graph ? renderGraph(graph) : <Typography>Loading...</Typography>}
+            {graph ? (
+              renderGraph(graph)
+            ) : (
+              <Typography>Loading {loaded}...</Typography>
+            )}
           </Paper>
         </Grid>
       </Grid>
