02c4194201df665bcca24d33f3173577fa3d6211
YoadNatan <yoad12121223@gmail.com>
2023-06-25 15:02:36 +0300

    CRITIs
    CritiQ Coming soon

--- src/app/components/pages/critiq/CritIQ.tsx
+++ src/app/components/pages/critiq/CritIQ.tsx
@@ -14,13 +14,19 @@ import IdeaSelector from "../../common/IdeaSelector";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 
+import RunCritIQ from "./RunCritIQ";
+import IdeaScore from "./IdeaScore";
+import ActionList from "./ActionList";
+import ValidationRoadmap from "./ValidationRoadmap";
+
 export default function Critiq() {
   const [Category, setCategory] = useState("");
   const [ideaId, setIdeaId] = useState<string>("");
-  const [value, setValue] = React.useState("one");
-  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-    setValue(newValue);
+  const [value, setValue] = React.useState("Run CritIQ");
+  const handleChange = (event: SelectChangeEvent) => {
+    setValue(event.target.value as string);
   };
+
   const [checkedState, setCheckedState] = useState({
     angel: false,
     vc: false,
@@ -43,6 +49,21 @@ export default function Critiq() {
     });
   };
 
+  const renderTabContent = () => {
+    switch (value) {
+      case "Run CritIQ":
+        return <RunCritIQ />;
+      case "Idea Score":
+        return <IdeaScore />;
+      case "Action List":
+        return <ActionList />;
+      case "Validation Roadmap":
+        return <ValidationRoadmap />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <>
       <Box>
@@ -58,16 +79,13 @@ export default function Critiq() {
           indicatorColor="primary"
           aria-label="ritIQNav"
         >
-          <Tab value="Run CritIQ" label="Run CritIQ" disabled={disabled} />
-          <Tab value="Idea Score" label="Idea Score" disabled={disabled} />
-          <Tab value="Action List" label="Action List" disabled={disabled} />
-          <Tab
-            value="Validation Roadmap"
-            label="Validation Roadmap "
-            disabled={disabled}
-          />
+          <Tab value="Run CritIQ" label="Run CritIQ" />
+          <Tab value="Idea Score" label="Idea Score" />
+          <Tab value="Action List" label="Action List" />
+          <Tab value="Validation Roadmap" label="Validation Roadmap " />
         </Tabs>
       </Box>
+      {renderTabContent()}
       <Grid
         container
         alignItems="center"
@@ -75,82 +93,7 @@ export default function Critiq() {
         justifyContent="center"
         spacing={2}
       >
-        <Grid item>
-          <Typography
-            align="center"
-            variant="h2"
-            sx={{ fontFamily: "sans-serif", margin: "15px" }}
-          >
-            CritIQ
-          </Typography>
-          <Typography align="center" variant="h6">
-            Let CritIQ test your idea
-          </Typography>
-        </Grid>
-        <Grid container item direction="row" spacing={2}>
-          <Grid item xs={6}>
-            <FormControl fullWidth>
-              <InputLabel id="idea">Idea</InputLabel>
-              <IdeaSelector
-                selectedIdeaId={ideaId}
-                setSelectedIdeaId={setIdeaId}
-              />
-            </FormControl>
-          </Grid>
-          <Grid item xs={6}>
-            <FormControl fullWidth>
-              <InputLabel id="category">Category</InputLabel>
-              <Select
-                labelId="category"
-                id="cat"
-                value={Category}
-                onChange={handleCategoryChange}
-                label="Category"
-              >
-                <MenuItem value={1}>Product</MenuItem>
-                <MenuItem value={2}>Business</MenuItem>
-                <MenuItem value={3}>Marketing</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-        </Grid>
-        <Grid item>
-          <FormGroup sx={{ display: "flex", flexDirection: "row" }}>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={checkedState.angel}
-                  onChange={handleCheckboxChange}
-                  name="angel"
-                />
-              }
-              label="Angel"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={checkedState.vc}
-                  onChange={handleCheckboxChange}
-                  name="vc"
-                />
-              }
-              label="VC"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={checkedState.customer}
-                  onChange={handleCheckboxChange}
-                  name="customer"
-                />
-              }
-              label="Customer"
-            />
-          </FormGroup>
-        </Grid>
-        <Grid item>
-          <Button variant="contained">Run CritIQ</Button>
-        </Grid>
+        8
       </Grid>
     </>
   );
---
+++ src/app/components/pages/critiq/ValidationRoadmap.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const RunCritIQ = () => {
+  // your component logic here...
+  return <div>{/* your JSX here... */}</div>;
+};
+
+export default RunCritIQ;
