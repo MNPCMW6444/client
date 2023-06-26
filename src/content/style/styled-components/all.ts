a36f0fe15d01339c50ff0c8d8e88bdac3ab90d53
ovaidov <106985580+YoadNatan@users.noreply.github.com>
2023-06-26 07:30:27 +0300

    da

--- src/content/style/styled-components/all.ts
+++ src/content/style/styled-components/all.ts
@@ -1,10 +1,36 @@
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import { styled } from "@mui/material";
 
 export const PromptButton = styled(Button)(({ theme }) => ({
-  color: "black" || theme.palette.primary.main,
+  color: "yellow" || theme.palette.primary.main,
+  backgroundColor: "transparent",
+  border: `2px solid ${theme.palette.primary.main}`,
+  borderRadius: "5px",
+  padding: "10px 20px",
+  "& .MuiButton-label": {
+    fontWeight: "bold",
+  },
+  "&:hover": {
+    backgroundColor: theme.palette.primary.main,
+    color: "white",
+  },
+  transition: "background-color 0.3s ease",
 }));
 
 export const LockedPromptButton = styled(Button)(({ theme }) => ({
   color: "gray" || theme.palette.primary.main,
+  backgroundColor: "transparent",
+  border: `2px solid ${theme.palette.primary.main}`,
+  borderRadius: "5px",
+  padding: "1px 5px",
+  "& .MuiButton-label": {
+    fontStyle: "italic",
+  },
+  "&:hover": {
+    backgroundColor: theme.palette.primary.main,
+    color: "white",
+  },
+  transition: "background-color 0.3s ease",
+  margin: "0 2px", 
 }));
+
