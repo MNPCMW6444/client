4de120d3d7e78ee89a69f7cacac9d2f626548aba
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-08-04 12:45:32 +0300

    awd

--- src/app/util/capitalize 2.ts
+++
@@ -1,8 +0,0 @@
-const capitalize = (s: string) =>
-  s
-    ? s
-        .replace(/([A-Z])/g, " $1")
-        .charAt(0)
-        .toUpperCase() + s.replace(/([A-Z])/g, " $1").slice(1)
-    : "NO STRING";
-export default capitalize;
