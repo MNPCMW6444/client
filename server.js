5ce525bba2fdc6fa8aa085f7ca7c9bfd514c4ed1
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-08-05 16:35:36 +0300

    wadf

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ app.use('/tos',
     express.static(path.join(__dirname, 'public', 'tos.html'))
 );
 
+console.log('process.env.NODE_ENV: ', process.env.NODE_ENV)
+
 // Production environment: serve the build
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'build')));
