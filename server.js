4e1fcd446f0abccebe1fbc01df4ebaba5069695b
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-08-05 16:20:04 +0300

    awd

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express =require(  "express");
+const express = require("express");
 const cors = require("cors");
-const path =require(  "path");
-const manifiestJSONData =require( "./src/manifiestJSONData");
+const path = require("path");
+const manifiestJSONData = require("./src/manifiestJSONData");
 require('dotenv').config();
 
 const app = express();
@@ -10,10 +10,7 @@ app.use(cors({
   origin: [process.env.REACT_APP_SERVER_URL]
 }));
 
-
 app.use('/', express.static(path.join(__dirname, 'website')));
-
-
 
 app.get('/manifest.json', (_, res) => {
     res.json(manifiestJSONData);
@@ -23,14 +20,25 @@ app.use('/tos',
     express.static(path.join(__dirname, 'public', 'tos.html'))
 );
 
+// Production environment: serve the build
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'build')));
+  
+  app.get('*', (_, res) => {
+      res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+} 
+// Development environment: forward the requests to localhost:5992
+else if (process.env.NODE_ENV === 'development') {
+    const { createProxyMiddleware } = require('http-proxy-middleware');
 
-app.use(express.static(path.join(__dirname, 'build')));
+  app.use(createProxyMiddleware({
+    target: 'http://localhost:5992',
+    changeOrigin: true
+  }));
+}
 
-app.get('*', (_, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-const port = process.env.PORT || ( 5999);
+const port = process.env.PORT || 5999;
 app.listen(port, "0.0.0.0");
 
 console.log('App is listening on port ' + port);
