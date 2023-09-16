0d7bac7bb11f7ea3cc70835cfdc6f056c70b1de1
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-09-16 12:54:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,45 +1,12 @@
-const express = require("express");
-const path = require("path");
-const manifestJSONData = require("./src/manifestJSONData");
-require('dotenv').config();
-
+const express = require('express');
 const app = express();
+const port = 3000;
 
-
-app.use('/', express.static(path.join(__dirname, 'website')));
-
-app.get('/manifest.json', (_, res) => {
-    res.json(manifestJSONData);
+app.get('/', (req, res) => {
+  // Redirect to the register page on failean.com
+  res.redirect('https://failean.com/register');
 });
 
-app.get('/tos', (_, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'tos.html'));
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
-
-app.get('/pay', (_, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'pay.html'));
-});
-
-
-// Production environment: serve the build
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'build')));
-
-    app.get('*', (_, res) => {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    });
-}
-// Development environment: forward the requests to localhost:5992
-else if (process.env.NODE_ENV === 'development') {
-    const {createProxyMiddleware} = require('http-proxy-middleware');
-
-    app.use(createProxyMiddleware({
-        target: 'http://localhost:5992',
-        changeOrigin: true
-    }));
-}
-
-const port = 5999;
-app.listen(port, "0.0.0.0");
-
-console.log('App is listening on port ' + port);
