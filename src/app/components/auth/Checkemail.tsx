2ee8aed624f26193e88fbd551f89fc042157ec02
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-09-17 13:17:16 +0300

    Update Checkemail.tsx

--- src/app/components/auth/Checkemail.tsx
+++ src/app/components/auth/Checkemail.tsx
@@ -1,11 +1,26 @@
-import { Typography } from "@mui/material";
+import {Typography} from "@mui/material";
+import Box from "@mui/material/Box";
+import {ToastContainer} from "react-toastify";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
 
 const Checkemail = () => {
-  return (
-    <Typography>
-      Check your email , you will receive a link to use failean!
-    </Typography>
-  );
+    return (
+        <Box width="100%" height="100%" bgcolor="black">
+            <ToastContainer/>
+            <Dialog open={true} onClose={() => {
+            }}>
+                <DialogTitle>Register</DialogTitle>
+                <DialogContent>
+                    <Typography>
+                        Check your email , you will receive a link to use failean!
+                    </Typography>
+                </DialogContent>
+            </Dialog>
+        </Box>
+
+    );
 };
 
 export default Checkemail;
