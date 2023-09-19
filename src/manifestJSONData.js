f7e003535e791d6eaa438fac3d93cea1e203c995
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-09-19 16:08:21 +0300

    vhjk

--- src/manifestJSONData.js
+++ src/manifestJSONData.js
@@ -2,36 +2,36 @@ require('dotenv').config();
 
 
 const env =
-  process.env.REACT_APP_WHITE_ENV === "dev"
-    ? " Dev"
-    : process.env.REACT_APP_WHITE_ENV === "tst"
-    ? " TST"
-    : "";
+    process.env.REACT_APP_WHITE_ENV === "dev"
+        ? " Dev"
+        : process.env.REACT_APP_WHITE_ENV === "tst"
+            ? " TST"
+            : "";
 
-    
+
 module.exports = {
-  short_name: `Failean${env}`,
-  name: `Failean App${env}`,
-  icons: [
-    {
-      src: "favicon.ico",
-      sizes: "1600x1600",
-      type: "image/x-icon",
-    },
-    {
-      src: "whiteLogo.png",
-      type: "image/png",
-      sizes: "1600x1600",
-    },
-    {
-      src: "whiteLogo144.png",
-      type: "image/png",
-      sizes: "144x144",
-    },
-  ],
-  start_url: "/",
-  background_color: "#ffffff",
-  display: "standalone",
-  scope: "/",
-  theme_color: "#ffffff",
+    short_name: `Failean${env}`,
+    name: `Failean App${env}`,
+    icons: [
+        {
+            src: "favicon.ico",
+            sizes: "1600x1600",
+            type: "image/x-icon",
+        },
+        {
+            src: "whiteIcon.png",
+            type: "image/png",
+            sizes: "1600x1600",
+        },
+        {
+            src: "whiteIcon.png",
+            type: "image/png",
+            sizes: "144x144",
+        },
+    ],
+    start_url: "/",
+    background_color: "#ffffff",
+    display: "standalone",
+    scope: "/",
+    theme_color: "#ffffff",
 };
