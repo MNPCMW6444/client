06ad00fa904c68ed4464c7c7898eb0fb66be484e
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-10-20 02:36:17 +0300

    wefwef

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+let emdot = "09:40";
+
+
+app.get('/getEmdot', (req, res) => 
+  res.status(200).send(emdot)
+);
+
+
 app.get('/', (req, res) => {
   // Redirect to the register page on failean.com
   res.redirect('https://failean.com');
